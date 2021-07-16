aef73dccad9967967cdda5b71b7d058f9818e948
RHKR_75 <khs20130731@gmail.com>
2021-07-16 12:59:42 +0900

    Dummy Data 추가

--- components/App/App.js
+++ components/App/App.js
@@ -3,20 +3,38 @@ import { Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Cart from "./components/Cart/Cart";
 import Main from "./components/Main/Main";
+import Item from "./components/Item/Item";
+import data from "MOCK_DATA.json"; // https://mockaroo.com/로부터 Dummy 데이터를 받아온다.
 
-// 다수의 component를 렌더링 하기 위해, 배열 형태로 component를 작성
-const App = props => [
-  <Nav key = {1} />,
-  <Routes key = {2} />
-];
+// App.js에서 dummy data를 불러옴으로써 최상단 컴포넌트로 잡고
+// state를 결정하기 위해 class 컴포넌트로 변경한다.
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // dummy data를 state로 지정
+      products: data
+    };
+  };
 
-const Routes = props => (
-  // Switch : 처음 매칭되는 Component만을 랜더링한다.
-  // Route : path의 url로 이동할 경우, 어떤 component를 랜더링할지 결정
-  <Switch>
-    <Route exact path = "/"  component = {Main} />
-    <Route exact path = "/cart" component = {Cart} />
-  </Switch>
-)
+  // React의 life cycle과 관련된 메소드로
+  // 컴포넌트가 DOM에 마운트 된 후 실행된다.
+  // App 컴포넌트가 실행될 때 어떠한 기능을 실행할지 결정
+  componentDidMount() {
+    console.log(this.state.products)
+  };
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path = "/"  component = {Main} />
+          <Route exact path = "/cart" component = {Cart} />
+        </Switch>
+      </div>
+    );
+  };
+};
 
 export default App;
